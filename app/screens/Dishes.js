dbf063ec9177f84e20eb44147f891e5c107a60f1
Xiao Kang <chenxk619@gmail.com>
2023-05-25 20:26:46 +0800

    Moved previous content to login.js and App.js is used for stack navigation now

--- app/screens/Dishes.js
+++ app/screens/Dishes.js
@@ -1,11 +1,13 @@
 import React, {useState} from 'react';
 import {SafeAreaView, Text} from 'react-native';
+import FlatButton from '../custom/Button';
 
-export default function Dishes() {
+export default function Dishes( {navigation} ) {
 
   return (
     <SafeAreaView>
-      <Text> Hello world </Text>
+      <Text> Dishes! </Text>
+      <FlatButton text = {'Reviews'} invert = {'n'} onPress={() => {navigation.navigate('Review')}}/>
     </SafeAreaView>
   );
 }
---
+++ app/screens/login.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react';
+import {SafeAreaView, Text, View, TextInput, Button} from 'react-native';
+import { globalStyles } from '../styles/globalStyles';
+import FlatButton from '../custom/Button';
+
+
+export default function Login( {navigation} ) {
+
+  const test = () => {
+    console.log(1);
+
+  }
+
+  return (
+    <SafeAreaView style = {globalStyles.container}>
+
+      <View style = {globalStyles.appHeader}>
+        <Text style = {globalStyles.appMainTitle}>FOOD4THOUGHT</Text>
+      </View>
+
+      <View style = {globalStyles.appBody}>
+        <Text style = {globalStyles.appBodyFont}> Say hello to endless culinary possibilities! </Text>
+      </View>
+
+      <View style = {globalStyles.appLogin}>
+        <Text style = {[globalStyles.appBodyFont, 
+          {fontSize: 35, fontWeight: '700', alignSelf: 'center', marginVertical: 15,}]}>
+          Login
+          </Text>
+        <TextInput style = {globalStyles.userInputs} placeholder='Email' />
+        <TextInput style = {globalStyles.userInputs} placeholder='Password'/>
+
+        <FlatButton text={'Sign In'} onPress={() => navigation.navigate('Dishes')} invert={'n'}/>
+        <FlatButton style = {{backgroundColor: 'white'}} text={'Forgot Password'} onPress={() => navigation.navigate('Dishes')} invert={'y'}/>
+
+        <Text style = {[globalStyles.appBodyFont, {fontSize: 15, marginTop: 200}]}>Don't have an account? Sign Up</Text>
+      </View>
+
+    </SafeAreaView>
+  );
+}

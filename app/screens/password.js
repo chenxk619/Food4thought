00ca991cae6bc658a6bc435a2f167094f790d9fe
BenjoBurger <benjaminggr@gmail.com>
2023-05-26 14:22:47 +0800

    Sign Up Created

---
+++ app/screens/password.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import {SafeAreaView, Text} from 'react-native';
+import FlatButton from '../custom/Button';
+
+export default function Password( {navigation} ) {
+
+  return (
+    <SafeAreaView>
+      <Text> Forgot Password! </Text>
+    </SafeAreaView>
+  );
+}
